d2b0d6f15688953c79b617ab9b10269412321d21
Songuos <songuos@qq.com>
2024-06-03 11:16:38 +0800

    message

---
+++ 19_文件处理进度更新_react/fileuploadlongrun/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+import './App.css'
+
+export default class App extends Component {
+
+    state = {
+        todos: [
+            { id: '001', name: '吃饭', done: true },
+            { id: '002', name: '睡觉', done: true },
+            { id: '003', name: '写代码', done: false },
+            { id: '004', name: '逛街', done: true }
+        ]
+    }
+
+
+    render() {
+        const { todos } = this.state
+        return (
+            <div>
+                <div className="todo-container">
+                    <div className="todo-wrap">
+                        <Header />
+                        <List todos={todos} />
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
