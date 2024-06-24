c6bad1fa062654941370a7b323385f678660542b
Songuos <songuos@qq.com>
2024-06-24 15:23:09 +0800

    message

---
+++ 19_文件处理进度更新_react/fileuploadlongrun/src/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+const DetailData = [
+    { id: '01', content: '你好，中国' },
+    { id: '02', content: '你好，世界' },
+    { id: '03', content: '你好，地球' },
+]
+
+
+
+export default class Detail extends Component {
+
+    render() {
+        const { id, title } = this.props.match.params
+        const findResult = DetailData.find((detailObj) => {
+            return detailObj.id === id
+        })
+        console.log(findResult)
+
+        return (
+            <div>
+                <ul>
+                    <li>ID: {id}</li>
+                    <li>Title: {title}</li>
+                    <li>Content: {findResult ? findResult.content : 'Not Found'}</li>
+                </ul>
+            </div >
+        )
+    }
+}
--- 19_文件处理进度更新_react/fileuploadlongrun/src/pages/Home/Message/index.jsx
+++ 19_文件处理进度更新_react/fileuploadlongrun/src/pages/Home/Message/index.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
+import Detail from './Detail'
 
 export default class Message extends Component {
+
+  state = {
+    messageArr: [
+      { id: '01', title: '消息001' },
+      { id: '02', title: '消息002' },
+      { id: '03', title: '消息003' },
+    ]
+  }
+
   render() {
     return (
       <div>
         <ul>
-          <li>
-            <a href="/message1">message001</a>&nbsp;&nbsp;
-          </li>
-          <li>
-            <a href="/message2">message002</a>&nbsp;&nbsp;
-          </li>
-          <li>
-            <a href="/message/3">message003</a>&nbsp;&nbsp;
-          </li>
+          {
+            this.state.messageArr.map((msgObj) => {
+              return (
+                <li key={msgObj.id}>
+                  <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
+                </li>
+              )
+            })
+          }
         </ul>
+        <hr />
+        <Route path="/home/message/detail/:id/:title" component={Detail} />
+
       </div>
     )
   }
