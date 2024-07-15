cd8556135e742230642342b44d1105cdd748b7b6
Songuos <songuos@qq.com>
2024-07-15 18:48:04 +0800

    redux

---
+++ 19_文件处理进度更新_react/fileuploadlongrun/src/components/Count/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+//引入store，用于获取redux中保存状态
+import store from '../../redux/store'
+
+export default class Count extends Component {
+
+
+    // // state = { count: 0 }
+    // componentDidMount() {
+    //     //订阅redux中状态的变化，只要状态发生变化，就调用回调函数
+    //     store.subscribe(() => {
+    //         this.setState({})
+    //     })
+    // }
+
+
+    increment = () => {
+        const { value } = this.selectNumber
+        // const { count } = this.state
+        // this.setState({ count: count + value * 1 })
+        // 通知redux加value
+        store.dispatch({ type: 'increment', data: value * 1 })
+    }
+    decrement = () => {
+        const { value } = this.selectNumber
+        store.dispatch({ type: 'decrement', data: value * 1 })
+    }
+
+    //当前和为奇数时再加
+    incrementIfOdd = () => {
+        const { value } = this.selectNumber
+        if (store.getState() % 2 === 1) {
+
+            store.dispatch({ type: 'increment', data: value * 1 })
+        }
+    }
+
+    //异步加
+    incrementAsync = () => {
+
+        const { value } = this.selectNumber
+        setTimeout(() => {
+            store.dispatch({ type: 'increment', data: value * 1 })
+        }, 500)
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>当前求和为：{store.getState()}</h1>
+                &nbsp;
+                <select ref={c => this.selectNumber = c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>
+            </div>
+        )
+    }
+}
